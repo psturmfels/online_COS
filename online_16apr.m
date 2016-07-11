2a3ba420138c9c69f9291d60c34be84ced2f4bad
Pascal Sturmfels <psturm@umich.edu>
2016-07-11 15:14:53 -0400

    began work on online cos

--- online_16apr.m
+++ online_16apr.m
@@ -1,4 +1,14 @@
-function [completion_times, weighted_sum] = online_16apr(p_times, weights, release_times)
+function weighted_sum = online_16apr(p_times, weights, release_times)
+k = 1;
+
+while ~isempty(p_times)
+    tk = 2^k - 1;
+    tk_plus1 = 2^(k+1) - 1;
+    interval_size = tk_plus1 - tk;
+    
+    
+    
+end
 
 
 end
