e6bc53db9d6314e10a71da8ca21e39b7ffcb3459
Pascal Sturmfels <psturm@umich.edu>
2016-07-12 14:10:03 -0400

    fixed issue with non-existent LP constriants

--- online_16apr.m
+++ online_16apr.m
@@ -20,35 +20,38 @@ while ~isempty(p_times)
     %Filter out jobs not yet released
     for i = 1:length(weights)
         if release_times(i) > tk
-           indices(i) = -1;
+            indices(i) = -1;
         end
     end
     indices = indices(indices ~= -1);
-    
-    %The set of jobs to consider in this interval
-    RA_tk = p_times(:, indices);
-    RA_weights = weights(indices);
-    
-    %Solve and round the LP
-    x_bar = COS_LP(RA_weights, RA_tk, interval_size);
-    x_bar_round = floor(x_bar + 0.5001);
-    scheduled_indices = indices(x_bar_round == 1);
-    
-    %Compute the weighted sum of completion times
-    for i = 1:length(scheduled_indices)
-        ct_i = max(sum(p_times(:, scheduled_indices(1:i)), 2)) + tk;
-        completion_times(ctindex(scheduled_indices(i))) = ct_i;
-        weighted_sum = weighted_sum + weights(scheduled_indices(i)) * (ct_i);
+    if ~isempty(indices)
+        %The set of jobs to consider in this interval
+        RA_tk = p_times(:, indices);
+        RA_weights = weights(indices);
+        
+        %Solve and round the LP
+        x_bar = COS_LP(RA_weights, RA_tk, interval_size);
+        x_bar_round = floor(x_bar + 0.5001);
+        scheduled_indices = indices(x_bar_round == 1);
+        
+        %Compute the weighted sum of completion times
+        for i = 1:length(scheduled_indices)
+            ct_i = max(sum(p_times(:, scheduled_indices(1:i)), 2)) + tk;
+            completion_times(ctindex(scheduled_indices(i))) = ct_i;
+            weighted_sum = weighted_sum + weights(scheduled_indices(i)) * (ct_i);
+        end
+        
+        %Compute necessary length of current interval
+        tk_plus1 = max(tk_plus1, tk + max(sum(p_times(:, scheduled_indices), 2)));
+        
+        %Drop scheduled jobs
+        ctindex(scheduled_indices) = [];
+        weights(scheduled_indices) = [];
+        release_times(scheduled_indices) = [];
+        p_times(:, scheduled_indices) = [];
+        
     end
     
-    %Compute necessary length of current interval
-    tk_plus1 = max(tk_plus1, tk + max(sum(p_times(:, scheduled_indices), 2)));
-   
-    %Drop scheduled jobs
-    ctindex(scheduled_indices) = [];
-    weights(scheduled_indices) = [];
-    release_times(scheduled_indices) = [];
-    p_times(:, scheduled_indices) = [];
     k = k + 1;
 end
 end
