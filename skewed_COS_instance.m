a306ee75cba8f98b5fa0a7d8df49e7d401642cf6
Pascal Sturmfels <psturm@umich.edu>
2016-07-14 13:34:15 -0400

    Added a way to generate only skewed jobs

---
+++ skewed_COS_instance.m
@@ -0,0 +1,11 @@
+function [p_times, weights, release_times] = skewed_COS_instance(num_jobs, num_machines, max_rt, max_p_time, max_weight)
+%Generate a COS instance with jobs that have 0 processing time on at 
+%least half of the machines
+p_times = zeros(num_machines, num_jobs);
+for i = 1:size(p_times, 2)
+   p_times(randsample(num_machines, floor(num_machines / 2)), i) = randi(max_p_time, floor(num_machines / 2), 1);
+end
+
+weights = randi(max_weight, num_jobs, 1);
+release_times = sort(randi(max_rt + 1, num_jobs, 1) - 1);
+end
