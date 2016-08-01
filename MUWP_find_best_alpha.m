f25ef272ed28616c0f68318dbf18a71f79b80292
jinglingli <renee.atbrynmawr@gmail.com>
2016-08-01 14:46:08 -0400

    modified output

--- MUWP_find_best_alpha.m
+++ MUWP_find_best_alpha.m
@@ -1,5 +1,5 @@
 function [subset] = MUWP_find_best_alpha(RA_weights, RA_tk, interval_size)
 [x_bar] = MUWP_LP(RA_weights, RA_tk, interval_size);
 [~, ind] = sort(-x_bar);
-[subset, ~, ~, ~, ~, ~] = find_best_step(x_bar, RA_weights, RA_tk, ind, interval_size);
+[~, ~, subset, ~, ~, ~] = find_best_step(x_bar, RA_weights, RA_tk, ind, interval_size);
 end
