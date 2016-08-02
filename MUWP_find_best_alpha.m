19e03a28aed7f8830fb8f4dff843e2cd44692f24
Pascal Sturmfels <psturm@umich.edu>
2016-08-02 09:31:08 -0400

    removed call to removed helper function

--- MUWP_find_best_alpha.m
+++ MUWP_find_best_alpha.m
@@ -7,7 +7,7 @@ end
 function [obj_val_round, mp_time_round, new_x_vals, temp_p_ratios, temp_w_ratios, round_ratios] = find_best_step(x_bar, weights, p_times, ind, max_processing_time)
     new_weights = weights(ind);
     new_p_times = p_times(:, ind);
-    obj_val_lin = get_obj_val(x_bar, weights);
+    obj_val_lin = dot(x_bar, 1 - weights);
     len = max(size(x_bar));
     temp_p_ratios = zeros(1, len);
     temp_w_ratios = zeros(1, len);
@@ -18,7 +18,7 @@ function [obj_val_round, mp_time_round, new_x_vals, temp_p_ratios, temp_w_ratios
     p_ratios = max((temp_p_ratios/max_processing_time), 1);
     w_ratios = max((temp_w_ratios/obj_val_lin), 1);
     round_ratios = p_ratios.*w_ratios;
-    [best_r, pos] = min(round_ratios);
+    [~, pos] = min(round_ratios);
     obj_val_round = temp_w_ratios(pos);
     mp_time_round = temp_p_ratios(pos);
     new_x_vals = zeros(1, len);
